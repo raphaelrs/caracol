a9b242bc2df680e69170ec0641a4dd98c20aec0b
Raphael Ribeiro da Silva <raphael_nino@hotmail.com>
2021-08-26 22:54:50 -0300

    iniciando projeto matriz caracol

---
+++ MatrizCaracol/Calculos.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MatrizCaracol
+{
+    public enum Direcao
+    {
+        Esquerda,
+        Cima,
+        Baixo,
+        Direita
+    }
+
+    public class Calculos
+    {
+        public static Direcao Direcao { get; set; }
+        public static int Linhas { get; set; }
+        public static int Colunas { get; set; }
+        public static int X { get; set; }
+        public static int Y { get; set; }
+        public static int?[,] Matriz { get; set; }
+
+
+        public static bool Salvar(int valor)
+        {
+            int xaux, yaux;
+
+            xaux = X;
+            yaux = Y;
+
+            if (Direcao == Direcao.Direita)
+            {
+                while (!EspacoLivre(xaux, yaux))
+                {
+                    yaux++;
+
+                    if (!IndicePertenceAMatriz(xaux, yaux))
+                    {
+                        yaux = Y; //diminui a coluna
+                        Direcao = Direcao.Baixo;
+                        break;
+                    }
+                }
+            }
+            else if (Direcao == Direcao.Baixo)
+            {
+                while (!EspacoLivre(xaux, yaux))
+                {
+                    xaux++;
+
+                    if (!IndicePertenceAMatriz(xaux, yaux))
+                    {
+                        xaux = X; //aumenta a linha
+                        Direcao = Direcao.Esquerda;
+                        break;
+                    }
+                }
+            }
+            else if (Direcao == Direcao.Cima)
+            {
+                while (!EspacoLivre(xaux, yaux))
+                {
+                    xaux--;
+
+                    if (!IndicePertenceAMatriz(xaux, yaux))
+                    {
+                        xaux = X; //aumenta a linha
+                        Direcao = Direcao.Direita;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                while (!EspacoLivre(xaux, yaux))
+                {
+                    yaux--;
+
+                    if (!IndicePertenceAMatriz(xaux, yaux))
+                    {
+                        yaux = Y;
+                        Direcao = Direcao.Cima;
+                        break;
+                    }
+                }
+            }
+
+            X = xaux;
+            Y = yaux;
+
+            if (Matriz[X, Y] == null)
+            {
+                Matriz[X, Y] = valor;
+                return true;
+            }
+
+            return false;
+        }
+
+        private static bool IndicePertenceAMatriz(int x, int y)
+        {
+            if (x >= 0 && x < Linhas && y >= 0 && y < Colunas)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static bool EspacoLivre(int x, int y)
+        {
+            if (Matriz[x, y] == null)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ MatrizCaracol/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MatrizCaracol
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string novo = "y";
+
+            while (novo == "y")
+            {
+                Console.WriteLine("Informe o numero de linhas : ");
+                Calculos.Linhas = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\n\n Informe o numero de colunas : ");
+                Calculos.Colunas = Convert.ToInt32(Console.ReadLine());
+
+                Calculos.Matriz = new int?[Calculos.Linhas, Calculos.Colunas];
+                Calculos.Direcao = Direcao.Direita;
+                Calculos.X = 0;
+                Calculos.Y = 0;
+
+                int qtd = Calculos.Linhas * Calculos.Colunas;
+
+
+                for (int i = 0; i < qtd; i++)
+                {
+                    if (!Calculos.Salvar(i))
+                    {
+                        i--;
+                    }
+                }
+
+                Console.Clear();
+                Console.Write(" ");
+
+                for (int i = 0; i < Calculos.Colunas; i++)
+                {                    
+                    Console.Write("____");
+                }
+                Console.WriteLine();
+                for (int i = 0; i < Calculos.Linhas; i++)
+                {
+                    Console.Write("| ");
+                    for (int j = 0; j < Calculos.Colunas; j++)
+                    {
+                        if (j < Calculos.Colunas -1)
+                        {
+                            if (Calculos.Matriz[i, j] < 10)
+                            {
+                                Console.Write(Calculos.Matriz[i, j] + "   ");
+                            }
+                            else
+                            {
+                                Console.Write(Calculos.Matriz[i, j] + "  ");
+                            }
+                        }
+                        else
+                        {
+                            Console.Write(Calculos.Matriz[i, j] + "  ");
+                        }
+                    }
+                    Console.Write("|\n");
+                }
+
+                Console.Write(" ");
+                for (int i = 0; i < Calculos.Colunas; i++)
+                {
+                    Console.Write("____");
+                }
+
+                Console.WriteLine("\n\nInforme y para continuar");
+                novo = Console.ReadLine();
+
+                Console.Clear();
+            } 
+        }
+    }
+}
